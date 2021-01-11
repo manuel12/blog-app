799778a297375533a57fc655971c094853adf476
Manuel <manuel@softgames.de>
2021-01-11 21:00:34 +0100

    Fixed settings file type

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
-    'accounts.apps.AccountsConfig'
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
